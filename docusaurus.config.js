0e42e22faf62f775f3edeae5fc2ebbb7a8a47f51
Wojciech Trocki <w.trocki@mongodb.com>
2025-07-10 12:09:32 +0200

    feat(docs): initialize Docusaurus config to render website (#4)

---
+++ docusaurus.config.js
@@ -0,0 +1,70 @@
+const { themes } = require("prism-react-renderer");
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "IPA - Improvement Proposal for APIs",
+  url: "https://go.mongodb.org", // Your website URL
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "mongodb",
+  projectName: "ipa", // Usually your repo name.
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: "ipa", // Use ipa/ as docs root
+          routeBasePath: "/", // Serve docs at site root
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: undefined,
+        },
+        blog: false,
+        theme: {},
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "IPA - Improvement Proposal for APIs",
+        items: [
+          {
+            href: "https://github.com/mongodb/ipa",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Documentation",
+            items: [
+              {
+                label: "General Guidelines",
+                to: "/1",
+              },
+              {
+                label: "SDK Guidelines",
+                to: "/900",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} MongoDB, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
